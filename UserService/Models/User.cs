394f45dfd537141fb34020590e8e6be45ed164a4
xxwonderwallxx <vika010501@gmail.com>
2024-06-21 14:14:50 -0400

    Init

---
+++ UserService/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserService.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [EmailAddress]
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Required]
+        public bool IsOnline { get; set; }
+        [ForeignKey(nameof(RoleId))]
+        public int RoleId { get; set; }
+
+        public virtual Role? Role { get; set; }
+        public virtual RegularUser? RegularUser { get; set; }
+        public virtual Admin? Admin { get; set; }
+    }
+}
